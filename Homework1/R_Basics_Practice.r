4b2fb9e36216d4d923fff27b6947fb8964b554fb
matthewep528 <matthewep528@gmail.com>
2024-09-26 14:02:53 -0400

    Changes to be committed: 	new file:   .gitignore 	new file:   .lintr 	new file:   Homework1/R_Basics_Practice.r 	new file:   Homework1/hw1.rmd 	new file:   Intro-to-R.Rproj 	new file:   shiny.pdf

---
+++ Homework1/R_Basics_Practice.r
@@ -0,0 +1,28 @@
+# loading required packages for rmd output
+require(knitr)
+require(rmdformats)
+knitr::opts_chunk$set(error = TRUE)
+
+# loading packages used in homework
+install.packages("ggplot2")
+install.packages("dplyr")
+
+library(ggplot2)
+library(dplyr)
+
+# Task 2: Creating Different Object Types
+# Creating numeric vector "my_numbers"
+
+my_numbers <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+# Creating vector of my five favorate colors
+
+my_colors <- c("green", "blue", "red", "purple", "orange")
+
+# Creating data frame of name, age, and grade
+
+name <- c("Guiseppe", "MargaretThatcher", "Theodore")
+age <- c(22, 87, 19)
+grade <- c("sophomore", "senior", "junior")
+
+student_info <- data.frame(name, age, grade)
